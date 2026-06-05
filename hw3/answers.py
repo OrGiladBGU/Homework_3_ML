ec214ab5506f5cda1c48c1943a32069d644eacc6
OrGiladBGU <orgila@post.bgu.ac.il>
2026-06-05 19:55:29 +0300

    commit2

--- hw3/answers.py
+++ hw3/answers.py
@@ -6,7 +6,7 @@ math (delimited with $$).
 """
 
 
-CUSTOM_DATA_URL = None
+CUSTOM_DATA_URL = "https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip"
 
 
 def vae_hyperparams():
