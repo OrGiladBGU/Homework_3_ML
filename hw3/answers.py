fe5733a9a0b3f1e8106e26e9cbd6d0a5eb39f381
OrGiladBGU <orgila@post.bgu.ac.il>
2026-06-05 19:31:17 +0300

    commit1

---
+++ hw3/answers.py
@@ -0,0 +1,44 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+
+CUSTOM_DATA_URL = None
+
+
+def vae_hyperparams():
+    hypers = dict(
+        batch_size=0, h_dim=0, z_dim=0, x_sigma2=0, learn_rate=0.0, betas=(0.0, 0.0),
+    )
+    # TODO: Tweak the hyperparameters to generate a former president.
+    # ====== YOUR CODE: ======
+
+    # ========================
+    return hypers
+
+
+part2_q1 = r"""
+**Your answer:**
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+"""
+
+part2_q4 = r"""
+**Your answer:**
+
+"""
+
+# ==============
